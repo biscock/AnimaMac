47673eb8f0dc65a3d40eb5360a14689d6d9f1d13
Seth <seth@visualcode.org>
2026-02-10 21:18:39 -0800

    fix some stuff, add speed slider

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use rfd::FileDialog;
 fn main() -> eframe::Result<()>{
     println!("AnimaTux");
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_decorations(false).with_transparent(true).with_has_shadow(false).with_inner_size([320.0,320.0]).with_movable_by_background(true).with_window_level(egui::WindowLevel::AlwaysOnTop),
+        vsync: false,
+        viewport: egui::ViewportBuilder::default().with_decorations(false).with_transparent(true).with_has_shadow(false).with_inner_size([320.0,320.0]).with_movable_by_background(true).with_window_level(egui::WindowLevel::AlwaysOnTop).with_mouse_passthrough(false),
         ..Default::default()
     };
 
@@ -26,11 +27,12 @@ struct AtApp<'a> {
     id: String,
     path: ImageSource<'a>,
     ids: Vec<String>,
-    ws: String
+    ws: String,
+    speed: i64
 }
 impl Default for AtApp<'_> {
     fn default() -> Self {
-        Self { uimode: false, id: "".to_owned(), ids: vec![], path: egui::include_image!("../assets/workplease.webp"), ws: "".to_owned()}
+        Self { uimode: false, id: "".to_owned(), ids: vec![], path: egui::include_image!("../assets/workplease.webp"), ws: "".to_owned(), speed:0}
     }
 }
 impl eframe::App for AtApp<'_> {
@@ -41,13 +43,15 @@ impl eframe::App for AtApp<'_> {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().frame(Frame::NONE).show(ctx, |ui| {
             if self.uimode{
-                #[cfg(feature = "lite")]{
+                let settings_frame = egui::Frame::default().fill(Color32::from_rgba_premultiplied(30, 27, 25, 255));
+                settings_frame.show(ui, |ui| {
+                    ui.heading("AnimaTux Settings");
+                    #[cfg(feature = "lite")]{
                     // Just gives a button for a file dialog, smaller unintended use
                     if ui.add(egui::Button::new("Select Animation/Image")).clicked(){
                         let _ = match FileDialog::new()
                                                     .add_filter("Image", &["png", "webp"])
                                                     .add_filter("Animation", &["webp", "gif"])
-                                                    //.set_directory("~")
                                                     .pick_file() {
                             Some(path) => {
                                 println!("{}", path.to_str().unwrap());
@@ -61,36 +65,42 @@ impl eframe::App for AtApp<'_> {
                 
                 
                 #[cfg(feature = "steamcmd")]{
-                        let settings_frame = egui::Frame::default().fill(Color32::from_rgba_premultiplied(30, 27, 25, 255));
-                        settings_frame.show(ui, |ui|{
-                        ui.heading("Download from Workshop");
-                        let id = ui.add(egui::TextEdit::singleline(&mut self.id));
-                        if id.lost_focus() && ui.input(|i|i.key_pressed(egui::Key::Enter)){
-                            //Attempt download
-                            workshop_dl(&self.id, &self.ws);
-                        }
-                        // WorkShop buttons (WIP)
                         
-                        
-                            ui.horizontal_wrapped(|ui|{
-                                for e in &self.ids{
-                                    if ui.add(egui::Button::new(e)).clicked(){
-                                        let path = format!(
-                                            "file:///home/seth/.local/share/Steam/steamapps/workshop/content/{}/{}",
-                                            include_str!("../assets/appid"),
-                                            e
-                                        );
-                                        println!("{}",path);
-                                        self.path = ImageSource::Uri(std::borrow::Cow::Owned(path));
-                                    }
+                            ui.vertical_centered(|ui| {
+                                ui.heading("Download from Workshop");
+                                let id = ui.add(egui::TextEdit::singleline(&mut self.id));
+                                if id.lost_focus() && ui.input(|i|i.key_pressed(egui::Key::Enter)){
+                                    //Attempt download
+                                    workshop_dl(&self.id, &self.ws);
                                 }
                             });
-                        });
+                            ui.horizontal_wrapped(|ui|{
+                                    for e in &self.ids{
+                                        if ui.add(egui::Button::new(e)).clicked(){
+                                            let path = format!(
+                                                "file://{}/{}",
+                                                get_ws(),
+                                                e
+                                            );
+                                            println!("{}",path);
+                                            self.path = ImageSource::Uri(std::borrow::Cow::Owned(path));
+                                        }
+                                    }
+                                });
+
+                            
                         
                         
                     }
+                    ui.label("Framerate (0 for default)");
+                    ui.add(egui::Slider::new(&mut self.speed, 0..=240));
+                });
+                
             }
-            ui.image(self.path.clone());
+            
+            let mut img = egui::Image::new(self.path.clone());
+            img.fps = u128::try_from(self.speed).unwrap();
+            ui.add(img);
             if ui.input(|i| i.key_released(egui::Key::Escape)) {
                 if self.uimode == false{
                     // Enable UI
